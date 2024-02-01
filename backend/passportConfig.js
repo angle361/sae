4879160f44cd8b6432406a17fc68c7c842788b92
angle361 <sanyam.student.mec19@itbhu.ac.in>
2024-02-02 00:47:09 +0530

    updated passportConfig

--- backend/passportConfig.js
+++ backend/passportConfig.js
@@ -5,7 +5,9 @@ const localStrategy = require("passport-local").Strategy;
 module.exports = function (passport) {
   passport.use(
     new localStrategy((username, password, done) => {
-      User.findOne({ username: username }, (err, user) => {
+
+      User.findOne({ username: username }).exec()
+      .then((err, user) => {
         if (err) throw err;
         if (!user) return done(null, false);
         
@@ -17,7 +19,12 @@ module.exports = function (passport) {
             return done(null, false);
           }
         });
+      })
+      .catch(err => {
+        console.error(err);
       });
+
+      
     })
   );
 
