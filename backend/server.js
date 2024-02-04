7b57467eb0d127832b81bd9e9a5d9276c7ca569a
angle361 <sanyam.student.mec19@itbhu.ac.in>
2024-02-05 00:34:45 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -21,12 +21,12 @@ app.use(bodyParser.json());
 
 // Middleware to set CORS headers
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://sae-iitbhu.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //database connection
