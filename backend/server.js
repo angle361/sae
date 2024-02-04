6aefdb9200fe0ad25569c09189f2cf5e31b785ee
angle361 <sanyam.student.mec19@itbhu.ac.in>
2024-02-05 00:04:26 +0530

    updated cors

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,14 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
-app.use(
-  cors()
-);
+
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
